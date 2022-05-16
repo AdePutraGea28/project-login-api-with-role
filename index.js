add9744a99fd7fe87b91a5ba71696c5553b6d570
adeputragea <adeputragea28@gmail.com>
2022-05-16 09:45:27 +0700

    connect to database mongodb

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/user_role_db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', (erro) => console.error(error));
+db.once('open', () => console.log('Database Connected...'));
 
 // middleware
 app.use(express.json());
